0401ad5ff7976fc9075e5d9d73840b35b61335ec
Dallas Lynn <dallas@dallaslynn.com>
2022-07-25 12:25:13 -0700

    Modify mix.exa for publishing

--- mix.exs
+++ mix.exs
@@ -16,6 +16,8 @@ defmodule AnnoyEx.MixProject do
       elixir: "~> 1.12",
       compilers: [:annoy, :elixir, :app],
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
 
       # Docs
@@ -43,6 +45,19 @@ defmodule AnnoyEx.MixProject do
     [
       {:random, "~> 0.2.4", only: :test},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "A NIF binding to Spotify's Annoy C++ library for approximate nearest neighbors."
+  end
+
+  defp package() do
+    [
+      maintainers: ["Dallas Lynn"],
+      files: ~w(lib priv .formatter.exs mix.exs README* LICENSE* src Makefile test),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/dallaselynn/annoy_ex"}
     ]
   end
 end
